e4c4fefac4dfb156b5636c81fcb55150e0d45752
Tiam Mühlenhöver <78230614+EndboyGames@users.noreply.github.com>
2023-08-21 16:28:13 +0200

    welcome

--- Main.py
+++ Main.py
@@ -1,2 +1,18 @@
-print("Welcome to PyRPG, a text based RPG written in Python.")
-gender =input("What´s your gender´? 1=Male 2=Female")
+#print welcome
+print("Welcome to PyRPG")
+
+#ask for name and gender
+
+gender = input("What´s your gender? 1 = male 2 = female")
+name = input("What´s your name?")
+
+#check if name and gender are correct
+
+if gender == 1:
+    print("You are male, right?")
+    input("Press 1 to confirm")
+    
+
+
+
+print("Welcome", name, "to the world of PyRPG.")
