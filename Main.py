4229844d93cb1849f4c83cdb115f5573de9608a3
Tiam Mühlenhöver <78230614+EndboyGames@users.noreply.github.com>
2023-08-21 16:47:11 +0200

    finished welcome instance

--- Main.py
+++ Main.py
@@ -2,31 +2,20 @@
 print("Welcome to PyRPG")
 
 #ask for name and gender
-
-gender = input("What´s your gender? 1 = male 2 = female ")
-name = input("What´s your name? ")
+gender = int(input("What's your gender? 1 = male 2 = female "))
+name = input("What's your name? ")
 
 #check if name and gender are correct
-
 if gender == 1:
-<<<<<<< HEAD
-   genderCorrectMale=input("You are male, right? 1 = yes 2 = no ")
-if genderCorrectMale == 2:
-    gender = 2
-if gender == 2:
-    genderCorrectFemale=input("You are female, right? 1 = yes 2 = no ")
-if genderCorrectFemale == 2:
-    gender = 1
-=======
-<<<<<<< HEAD
-    gendercorrect=input("You are male, right? 1 = yes 2 = no")
-
-=======
-    print("You are male, right?")
-    input("Press 1 to confirm")
-    
->>>>>>> e4c4fefac4dfb156b5636c81fcb55150e0d45752
->>>>>>> 722baa9fd6e8afaff87b2b239a1a88687026af20
-
-
-print("Welcome to the world of PyRPG.", name, "!")
+    genderCorrectMale = input("You are male, right? 1 = yes 2 = no ")
+    if genderCorrectMale == "2":
+        gender = 2
+elif gender == 2:
+    genderCorrectFemale = input("You are female, right? 1 = yes 2 = no ")
+    if genderCorrectFemale == "2":
+        gender = 1
+        
+if gender == 1:
+    print("Welcome to the world of PyRPG, male", name, "!")
+elif gender == 2:
+    print("Welcome to the world of PyRPG, female", name, "!")
