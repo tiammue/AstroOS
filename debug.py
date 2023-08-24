69006ade11d6419fb325af4a1b230ac78fc1cf53
Tiam Mühlenhöver <78230614+EndboyGames@users.noreply.github.com>
2023-08-24 18:02:45 +0200

    nevermind, using shelve

--- debug.py
+++ debug.py
@@ -1,6 +1,17 @@
-import dill                            #pip install dill --user
-filename = 'globalsave.pkl'
-dill.dump_session(filename)
+import shelve
 
-# and to load the session again:
-dill.load_session(filename)
+T='Hiya'
+val=[1,2,3]
+
+filename='/tmp/shelve.out'
+my_shelf = shelve.open(filename,'n') # 'n' for new
+
+for key in dir():
+    try:
+        my_shelf[key] = globals()[key]
+    except TypeError:
+        #
+        # __builtins__, my_shelf, and imported modules can not be shelved.
+        #
+        print('ERROR shelving: {0}'.format(key))
+my_shelf.close()
