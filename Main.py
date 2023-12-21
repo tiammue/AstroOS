7a0fd6947a0eb96c2494ccdb87fa9badd2069f8f
tiammue <78230614+tiammue@users.noreply.github.com>
2023-12-21 14:27:00 +0100

    remove unnacesery stuff

--- Main.py
+++ Main.py
@@ -17,36 +17,3 @@ print("                 |/\\/\\/\\/\\/|")
 
 print("         ©Endboy Interactive Studio 2023")
 print("")
-#commented out the code below because it gave an error
-#remove the 3 " at the start and end of the error code to uncomment the code
-"""
-print("Loading User Accounts...")
-
-#Load Username with pickle
-
-with open('Save.pkl', 'rb') as f:
-
-    Username_loaded = pickle.load(f) # deserialize using load()
-    input = input("The current User is "+ Username_loaded + ". Is this correct? y/n:")
-
-if input == "y":
- Username = Username_loaded
-else:
-   input = input("Do you wish to erase all data and login? y/n:")
-   if input == "y":
-    Username = input("Input new username:")
-      #there was an "else" here but it gave an error so i removed it
-
-# open a text file
-
-with open('Save.pkl', 'wb') as f:  
-    pickle.dump(Username, f) # serialize the list
-
-f.close
-
-with open('Save.pkl', 'rb') as f:
-
-    Username_loaded = pickle.load(f) # deserialize using load()
-    print(Username_loaded)
-
-"""
